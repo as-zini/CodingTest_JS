e0ecb56807f9be8e4a5eb0b558151231006c5c77
as-zini <aszini@naver.com>
2025-04-13 09:58:35 +0900

    [level 0] Title: 중복된 문자 제거, Time: 0.18 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.js
@@ -0,0 +1,11 @@
+function solution(my_string) {
+    var answer = '';
+    var my_string = my_string.split("");
+    var result = [];
+    for(let i = 0; i < my_string.length; i++){
+        if(!result.includes(my_string[i])){
+            result.push(my_string[i])
+        }
+    }
+    return result.join("");
+}
