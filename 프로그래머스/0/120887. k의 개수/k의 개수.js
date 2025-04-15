de783c6d5788e51af5fb1bccfd08700962f38ddb
as-zini <aszini@naver.com>
2025-04-15 23:08:53 +0900

    [level 0] Title: k의 개수, Time: 28.84 ms, Memory: 41 MB -BaekjoonHub

---
+++ 프로그래머스/0/120887. k의 개수/k의 개수.js
@@ -0,0 +1,23 @@
+// function solution(i, j, k) {
+//     var answer = 0;
+//     for(let x = i; x <= j; x++){
+//         if(String(x).includes(String(k))){
+//             var result = String(x).replaceAll(String(k), "/")
+//             console.log(result.split("/"))
+//             console.log(result.length)
+//             answer += result.length-1
+//         }
+//     }
+//     return answer;
+// }
+
+function solution(i, j, k) {
+    let count = 0;
+    const target = k.toString(); // 숫자 k를 문자열로 변환
+    
+    for (let num = i; num <= j; num++) {
+        count += num.toString().split('').filter(digit => digit === target).length;
+    }
+    
+    return count;
+}
