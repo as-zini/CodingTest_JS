7d116a12adf9d0c83def5696736177a5934fb4b8
as-zini <aszini@naver.com>
2025-05-09 18:31:25 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
@@ -1,10 +1,20 @@
-function solution(my_string, num1, num2) {
-    var answer = "";
-    var my_string = my_string.split("");
-    var first_char = my_string[num1];
-    var answer = my_string.splice(num1,1,my_string[num2])
-    var answer = my_string.splice(num2,1,first_char)
-    console.log(my_string)
-    
-    return my_string.join("");
+function solution(my_string, num1, num2){
+    var str1 = my_string[num1]
+    var str2 = my_string[num2]
+    var newString = my_string.slice(0,num1)+str2+my_string.slice(num1+1,num2)+str1+my_string.slice(num2+1)
+    return newString
 }
+
+
+
+
+// function solution(my_string, num1, num2) {
+//     var answer = "";
+//     var my_string = my_string.split("");
+//     var first_char = my_string[num1];
+//     var answer = my_string.splice(num1,1,my_string[num2])
+//     var answer = my_string.splice(num2,1,first_char)
+//     console.log(my_string)
+    
+//     return my_string.join("");
+// }
