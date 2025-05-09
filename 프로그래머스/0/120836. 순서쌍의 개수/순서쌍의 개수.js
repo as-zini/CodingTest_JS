f4f416c5695e227380bc8df713e07f64162b329e
as-zini <aszini@naver.com>
2025-05-09 18:16:12 +0900

    [level 0] Title: 순서쌍의 개수, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.js
+++ 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.js
@@ -1,11 +1,28 @@
-function solution(n) {
-    let count = 0;
-    
-    for (let i = 1; i <= n; i++) {
-        if (n % i === 0) { // i가 n의 약수인지 확인
-            count++; // 순서쌍 (i, n / i) 추가
+function solution(n){
+    var answer = 0
+    for(let i = 1;i*i<=n;i++){
+        if(n%i === 0){
+            if(i*i === n)answer+=1
+            else answer += 2
         }
+        
     }
-    
-    return count;
+    return answer
 }
+
+
+
+
+
+
+// function solution(n) {
+//     let count = 0;
+    
+//     for (let i = 1; i <= n; i++) {
+//         if (n % i === 0) { // i가 n의 약수인지 확인
+//             count++; // 순서쌍 (i, n / i) 추가
+//         }
+//     }
+    
+//     return count;
+// }
