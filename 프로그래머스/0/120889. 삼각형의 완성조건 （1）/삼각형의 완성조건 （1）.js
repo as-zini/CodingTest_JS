938069ac8f45735fbbb052a497495acf866f6207
as-zini <aszini@naver.com>
2025-05-09 18:34:50 +0900

    [level 0] Title: 삼각형의 완성조건 (1), Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.js
+++ 프로그래머스/0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.js
@@ -1,18 +1,28 @@
-function solution(sides) {
-    var max = Math.max(...sides);
-    var count = 0;
-    var same = 0;
-    sides.forEach((el) => {
-        if(el<max){
-            count = count + el;
-        }else if(el = max){
-            same++;
-        }
-    })
-    
-    var answer = count > max ? 1 : 2;
-    if(same>=2){
-        answer = 1;
-    }
-    return answer;
+function solution(sides){
+    var sorted = [...sides].sort((a,b) => b-a)
+    return sorted[0] < sorted[1]+sorted[2] ? 1 : 2
 }
+
+
+
+
+
+
+// function solution(sides) {
+//     var max = Math.max(...sides);
+//     var count = 0;
+//     var same = 0;
+//     sides.forEach((el) => {
+//         if(el<max){
+//             count = count + el;
+//         }else if(el = max){
+//             same++;
+//         }
+//     })
+    
+//     var answer = count > max ? 1 : 2;
+//     if(same>=2){
+//         answer = 1;
+//     }
+//     return answer;
+// }
