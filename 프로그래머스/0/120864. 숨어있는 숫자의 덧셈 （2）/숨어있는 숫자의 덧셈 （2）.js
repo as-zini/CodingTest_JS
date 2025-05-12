10921e66d4e72319f80f8353a5fe7728337c56e6
as-zini <aszini@naver.com>
2025-05-12 11:31:37 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.20 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
@@ -1,3 +1,18 @@
-function solution(my_string) {
-    return (my_string.match(/\d+/g) || []).reduce((sum, num) => sum + Number(num), 0);
+function solution(my_string){
+    const reg = /[\D+]/g
+    return my_string.split(reg).reduce((sum,el) => el ? sum+=Number(el) : sum, 0)
 }
+
+
+
+
+
+
+
+
+
+
+
+// function solution(my_string) {
+//     return (my_string.match(/\d+/g) || []).reduce((sum, num) => sum + Number(num), 0);
+// }
