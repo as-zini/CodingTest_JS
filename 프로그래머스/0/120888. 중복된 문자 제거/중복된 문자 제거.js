998600f9c98c19a0fa03630a9a0e113bf9a5c4d8
as-zini <aszini@naver.com>
2025-05-12 11:00:16 +0900

    [level 0] Title: 중복된 문자 제거, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.js
+++ 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.js
@@ -1,11 +1,26 @@
-function solution(my_string) {
-    var answer = '';
-    var my_string = my_string.split("");
-    var result = [];
-    for(let i = 0; i < my_string.length; i++){
-        if(!result.includes(my_string[i])){
-            result.push(my_string[i])
-        }
-    }
-    return result.join("");
+function solution(my_string){
+    var set = new Set(my_string.split(""))
+    return [...set].join("")
+    
 }
+
+
+
+
+
+
+
+
+
+
+// function solution(my_string) {
+//     var answer = '';
+//     var my_string = my_string.split("");
+//     var result = [];
+//     for(let i = 0; i < my_string.length; i++){
+//         if(!result.includes(my_string[i])){
+//             result.push(my_string[i])
+//         }
+//     }
+//     return result.join("");
+// }
