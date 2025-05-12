1a5e484114c426f51c5fd3bd17cfe87958c8987b
as-zini <aszini@naver.com>
2025-05-12 10:53:12 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.04 ms, Memory: 33.2 MB -BaekjoonHub

--- 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.js
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.js
@@ -1,12 +1,27 @@
-function solution(n) {
-    let pizza = 1;
-    while ((pizza * 6) % n !== 0) { // 6조각씩 있는 피자가 n명의 배수가 될 때까지 반복
-        pizza++;
+function solution(n){
+    var count = 1
+    while(true){
+        if((n*count)%6 === 0)break
+        else count+=1
     }
-    return pizza;
+    return (n*count)/6
 }
 
-// 실행 예제
-console.log(solution(6));  // 1
-console.log(solution(10)); // 5
-console.log(solution(4));  // 2
+
+
+
+
+
+
+// function solution(n) {
+//     let pizza = 1;
+//     while ((pizza * 6) % n !== 0) { // 6조각씩 있는 피자가 n명의 배수가 될 때까지 반복
+//         pizza++;
+//     }
+//     return pizza;
+// }
+
+// // 실행 예제
+// console.log(solution(6));  // 1
+// console.log(solution(10)); // 5
+// console.log(solution(4));  // 2
