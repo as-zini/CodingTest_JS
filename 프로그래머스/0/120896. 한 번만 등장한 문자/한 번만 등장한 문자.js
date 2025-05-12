63fefa2d8a29abd4d7c63c8909040b119aceeca1
as-zini <aszini@naver.com>
2025-05-12 11:35:41 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.js
+++ 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.js
@@ -1,13 +1,25 @@
-function solution(s) {
-    
-    var result = {};
-    var string_list = s.split("");
-    string_list.forEach((el) =>{
-        result[el] = (result[el] || 0)+1;
+function solution(s){
+    var list = s.split("").sort()
+    var answer = []
+    list.forEach((el,index) => {
+        if(el !== list[index+1] && el !== list[index-1])answer.push(el)
     })
-    var real_answer = []
-    var answer = Object.entries(result).filter((el) => el[1]===1)
-    answer.forEach((el) => real_answer.push(el[0]))
-    answer = real_answer.sort().join("")
-    return answer;
+    return answer.join("")
 }
+
+
+
+
+// function solution(s) {
+    
+//     var result = {};
+//     var string_list = s.split("");
+//     string_list.forEach((el) =>{
+//         result[el] = (result[el] || 0)+1;
+//     })
+//     var real_answer = []
+//     var answer = Object.entries(result).filter((el) => el[1]===1)
+//     answer.forEach((el) => real_answer.push(el[0]))
+//     answer = real_answer.sort().join("")
+//     return answer;
+// }
