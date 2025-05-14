b0b085941f281671184c5799d189012b751b0cae
as-zini <aszini@naver.com>
2025-05-14 23:10:53 +0900

    [level 0] Title: 이진수 더하기, Time: 2.67 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120885. 이진수 더하기/이진수 더하기.js
+++ 프로그래머스/0/120885. 이진수 더하기/이진수 더하기.js
@@ -1,11 +1,27 @@
-function solution(bin1, bin2) {
-  // 2진수를 10진수로 변환 후 더하기
-  const sum = parseInt(bin1, 2) + parseInt(bin2, 2);
-  
-  // 다시 2진수 문자열로 변환하여 반환
-  return sum.toString(2);
+function solution(bin1, bin2){
+    var num1 = parseInt(bin1, 2)
+    var num2 = parseInt(bin2, 2)
+    console.log(num1,num2)
+    var answer = (num1+num2).toString(2)
+    return answer
 }
 
-// 테스트
-console.log(solution("10", "11"));    // "101"
-console.log(solution("1001", "1111")); // "11000"
+
+
+
+
+
+
+
+
+// function solution(bin1, bin2) {
+//   // 2진수를 10진수로 변환 후 더하기
+//   const sum = parseInt(bin1, 2) + parseInt(bin2, 2);
+  
+//   // 다시 2진수 문자열로 변환하여 반환
+//   return sum.toString(2);
+// }
+
+// // 테스트
+// console.log(solution("10", "11"));    // "101"
+// console.log(solution("1001", "1111")); // "11000"
