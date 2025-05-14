6eb48c6ef18bd5d5046f0d9b4b0d71563d575b21
as-zini <aszini@naver.com>
2025-05-14 22:56:02 +0900

    [level 0] Title: A로 B 만들기, Time: 0.75 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.js
+++ 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.js
@@ -1,5 +1,17 @@
-
-
-function solution(before, after) {
-    return before.split("").sort().join("") === after.split("").sort().join("") ? 1 : 0;
+function solution(before, after){
+    var before = before.split("").sort().join("")
+    var after = after.split("").sort().join("")
+    return before === after ? 1 : 0
 }
+
+
+
+
+
+
+
+
+
+// function solution(before, after) {
+//     return before.split("").sort().join("") === after.split("").sort().join("") ? 1 : 0;
+// }
