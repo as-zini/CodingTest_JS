3a88098f3d4de1bdcb0c435db1cde5581fbd4e14
as-zini <aszini@naver.com>
2025-05-14 23:14:35 +0900

    [level 0] Title: 소인수분해, Time: 0.31 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120852. 소인수분해/소인수분해.js
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.js
@@ -1,15 +1,39 @@
-function solution(n) {
-    let factors = new Set();
-    let divisor = 2;
-    
-    while (n >= 2) {
-        if (n % divisor === 0) {
-            factors.add(divisor);
-            n /= divisor;
-        } else {
-            divisor++;
+function solution(n){
+    var answer = new Set()
+    var num = n
+    var current = 2
+    while(num !== 1){
+        if(num%current === 0){
+            answer.add(current)
+            num = num/current
+        }else{
+            current += 1
         }
     }
-    
-    return [...factors];
+    return [...answer]
 }
+
+
+
+
+
+
+
+
+
+
+// function solution(n) {
+//     let factors = new Set();
+//     let divisor = 2;
+    
+//     while (n >= 2) {
+//         if (n % divisor === 0) {
+//             factors.add(divisor);
+//             n /= divisor;
+//         } else {
+//             divisor++;
+//         }
+//     }
+    
+//     return [...factors];
+// }
