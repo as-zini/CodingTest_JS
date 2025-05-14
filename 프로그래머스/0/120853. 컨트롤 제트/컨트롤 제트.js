f569786f5bca0ee86534dde1f8829d5578427ba4
as-zini <aszini@naver.com>
2025-05-14 23:02:04 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.js
+++ 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.js
@@ -1,18 +1,36 @@
-function solution(s) {
-    var char_list = s.split(" ");
-    var cost = 0;
-    var before_num = 0;
-    var answer = char_list.forEach((el) => {
-        if(el === "Z"){
-            cost = cost - before_num;
-            
-        }else{
-            cost = cost + Number(el);
-            before_num = Number(el);
-            
+function solution(s){
+    var answer = 0
+    s.split(" ").forEach((el,index,arr) => {
+        if(arr[index+1] !== "Z" && el !== 'Z'){
+            answer += Number(el)
         }
-        
     })
-    
-    return cost;
+    return answer
 }
+
+
+
+
+
+
+
+
+
+// function solution(s) {
+//     var char_list = s.split(" ");
+//     var cost = 0;
+//     var before_num = 0;
+//     var answer = char_list.forEach((el) => {
+//         if(el === "Z"){
+//             cost = cost - before_num;
+            
+//         }else{
+//             cost = cost + Number(el);
+//             before_num = Number(el);
+            
+//         }
+        
+//     })
+    
+//     return cost;
+// }
