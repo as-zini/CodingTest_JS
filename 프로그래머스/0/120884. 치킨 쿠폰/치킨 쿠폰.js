5764fb13da450095a9c711c4f56659f4551cf3f0
as-zini <aszini@naver.com>
2025-05-15 12:23:46 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.js
+++ 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.js
@@ -1,16 +1,32 @@
-function solution(chicken) {
-  let serviceChicken = 0;
-  let coupon = chicken;
-
-  while (coupon >= 10) {
-    let newChicken = Math.floor(coupon / 10); // 서비스 치킨 수
-    serviceChicken += newChicken;
-    coupon = (coupon % 10) + newChicken; // 남은 쿠폰 + 새로 받은 쿠폰
-  }
-
-  return serviceChicken;
+function solution(chicken){
+    var coupon = chicken
+    var service = 0;
+    while(coupon >= 10){
+        service += Math.floor(coupon/10)
+        coupon = Math.floor(coupon/10) + coupon%10
+    }
+    return service
 }
 
-// 테스트
-console.log(solution(100));  // 11
-console.log(solution(1081)); // 120
+
+
+
+
+
+
+// function solution(chicken) {
+//   let serviceChicken = 0;
+//   let coupon = chicken;
+
+//   while (coupon >= 10) {
+//     let newChicken = Math.floor(coupon / 10); // 서비스 치킨 수
+//     serviceChicken += newChicken;
+//     coupon = (coupon % 10) + newChicken; // 남은 쿠폰 + 새로 받은 쿠폰
+//   }
+
+//   return serviceChicken;
+// }
+
+// // 테스트
+// console.log(solution(100));  // 11
+// console.log(solution(1081)); // 120
